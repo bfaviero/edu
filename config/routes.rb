ddacfc21d2187bbd645ab489dd2a2b61a7005b57
Bruno <bfaviero@mit.edu>
2013-01-20 11:17:18 -0500

    finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Edu::Application.routes.draw do
-  get "static_pages/home"
-  root              to: "welcome#index"
+  get "users/new"
+
+  root              to: "static_pages#home"
+  match '/signup/', to: 'users#new'
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
